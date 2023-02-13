75ae10c25257dbc056cf255ca0759a60d0bc16b1
Avinash Saroj <you@example.com>
2023-02-14 02:18:06 +0530

    first commit

---
+++ Dicee Challenge - Starting Files/dicee.js
@@ -0,0 +1,35 @@
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
+var randomImage = "dice" + randomNum1 + ".png";
+var randomImageSource = "/FSD-1/Dicee Challenge - Starting Files copy/Dicee Challenge - Starting Files/images/"
+  + randomImage;
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src", randomImageSource);
+
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "dice" + randomNum2 + ".png";
+var randomImageSource2 = "/FSD-1/Dicee Challenge - Starting Files copy/Dicee Challenge - Starting Files/images/" + randomImage2;
+
+var img2 = document.querySelectorAll("img")[1];
+img2.setAttribute("src", randomImageSource2);
+
+
+
+ if(randomImageSource > randomImageSource2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 WON!🚩";
+
+}
+else if (randomImageSource < randomImageSource2)
+{
+  document.querySelector("h1").innerHTML = "Player 2 WON!🚩";
+ 
+}
+
+
+else
+{
+  document.querySelector("h1").innerHTML = "Draw !";
+ 
+}
